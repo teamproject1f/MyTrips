727350c3480111bfd9f744f88d51245fdacf0ac2
Husseinhtm99 <husseinhtm99@gmail.com>
2022-11-02 20:22:43 +0200

    log

---
+++ lib/log.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class log extends StatelessWidget {
+  const log({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
