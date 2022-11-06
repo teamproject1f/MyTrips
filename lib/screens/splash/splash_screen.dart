bf030956883c293a2d287c65de8bdbea86a96e1f
Husseinhtm99 <husseinhtm99@gmail.com>
2022-11-06 16:48:34 +0200

    splash//Hussein

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:team1/screens/home/home_screen.dart';
 import 'package:team1/screens/on_boarding/on_boarding_screen.dart';
 import 'package:team1/shared/styles/colors.dart';
 import 'package:team1/shared/styles/styles.dart';
-
-import '../../layout/home_layout.dart';
 import '../../shared/components/components.dart';
 import '../../shared/components/constants.dart';
 
@@ -23,7 +22,6 @@ class SplashScreen extends StatelessWidget {
 class SplashScreenBody extends StatefulWidget {
   const SplashScreenBody({Key? key}) : super(key: key);
 
-
   @override
   State<SplashScreenBody> createState() => _SplashScreenBodyState();
 }
@@ -39,7 +37,7 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
     animationController =
         AnimationController(vsync: this, duration: const Duration(seconds: 5));
     fadingAnimation =
-        Tween<double>(begin: 0.5, end: 1).animate(animationController!);
+        Tween<double>(begin: 0.1, end: 1).animate(animationController!);
 
     animationController?.repeat(reverse: true);
 
@@ -55,55 +53,63 @@ class _SplashScreenBodyState extends State<SplashScreenBody>
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    return Stack(
-        children: [
-      Image.asset(splashImage),
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          FadeTransition(
-            opacity: fadingAnimation!,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'TRI',
-                  style: GoogleFonts.pacifico(
-                    textStyle: const TextStyle(
-                      color: defaultColor,
-                      fontSize: 45,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                Text(
-                  'PSO',
-                  style: GoogleFonts.pacifico(
-                    textStyle: const TextStyle(
-                      color: defaultWhiteColor,
-                      fontSize: 45,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+    return Container(
+      constraints: const BoxConstraints.expand(),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(splashImage),
+          fit: BoxFit.cover,
+        ),
       ),
-
-    ]);
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: defaultAppBar(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FadeTransition(
+              opacity: fadingAnimation!,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'TRI',
+                    style: GoogleFonts.pacifico(
+                      textStyle: const TextStyle(
+                        color: defaultColor,
+                        fontSize: 45,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    'PSO',
+                    style: GoogleFonts.pacifico(
+                      textStyle: const TextStyle(
+                        color: defaultWhiteColor,
+                        fontSize: 45,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   void goToNextView() {
     Widget widget;
     Future.delayed(const Duration(seconds: 6), () {
       if (uId != null) {
-        widget =  const HomeLayout();
-      }else {
+        widget = const HomeScreen();
+      } else {
         widget = const OnBoard();
       }
-      navigateAndFinish(context,widget);
+      navigateAndFinish(context, widget);
     });
   }
 }
