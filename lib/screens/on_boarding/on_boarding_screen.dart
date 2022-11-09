c3662f00c8a8f38b0c4228bee2b5c4c59126af0f
Husseinhtm99 <husseinhtm99@gmail.com>
2022-11-09 23:58:05 +0200

    moreUpdate//Hussein

--- lib/screens/on_boarding/on_boarding_screen.dart
+++ lib/screens/on_boarding/on_boarding_screen.dart
@@ -29,18 +29,15 @@ class OnBoard extends StatelessWidget {
             backgroundColor: Colors.transparent,
             body: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 50.0),
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    space(0, 186),
-                    welcomeText(),
-                    space(0, 89),
-                    button(context),
-                    space(0, 186),
-
-                  ],
-                ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  space(0, 186),
+                  welcomeText(),
+                  space(0, 89),
+                  button(context),
+                  space(0, 186),
+                ],
               ),
             ),
           ),
